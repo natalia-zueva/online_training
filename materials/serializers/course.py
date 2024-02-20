27153f8af93454568b23f73e1e7606ea945deb12
Natalia Zueva <zueva_natalia@mail.ru>
2024-02-20 12:02:51 +0500

    lessons_count

--- materials/serializers/course.py
+++ materials/serializers/course.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
-from materials.models import Course
+from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_in_course = SerializerMethodField()
+
+    def get_lessons_in_course(self, lesson):
+        return Lesson.objects.filter(course=lesson.course).count()
+
     class Meta:
         model = Course
         fields = '__all__'
+
+
--- materials/serializers/lesson.py
+++ materials/serializers/lesson.py
@@ -4,6 +4,7 @@ from materials.models import Lesson
 
 
 class LessonSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Lesson
         fields = '__all__'
