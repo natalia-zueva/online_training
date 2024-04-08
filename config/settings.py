03e032fd49b2fb6a0a9c4eff5a2aa2ae3e811586
Natalia Zueva <zueva_natalia@mail.ru>
2024-04-08 10:24:08 +0500

    Fixing environment in docker-compose

--- config/settings.py
+++ config/settings.py
@@ -88,9 +88,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'traningdocker',
-        'USER': 'postgres',
-        'PASSWORD': 'mysecretpassword',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': 'db',
         'PORT': 5432
     }
@@ -160,8 +160,8 @@ REST_FRAMEWORK = {
 
 STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
 
-CELERY_BROKER_URL = 'redis://redis:6379/0'
-CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
 
 
 CELERY_BEAT_SCHEDULE = {
